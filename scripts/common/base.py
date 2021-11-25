e6ce45d525ecad6374ebd0deb40e15d4362b3873
Gokhan KESLER <gokhankesler@gmail.com>
2021-11-26 00:25:46 +0300

    Add version 1.0

--- scripts/common/base.py
+++ scripts/common/base.py
@@ -3,7 +3,7 @@ from sqlalchemy.orm import Session
 from sqlalchemy.orm import declarative_base
 
 engine = create_engine(
-    'postgresql+psycopg2://user:password*@localhost:1234/database_name'
+    'postgresql+psycopg2://postgres:password@localhost:1234/postgres'
 )
 session = Session(engine)
 Base = declarative_base()
